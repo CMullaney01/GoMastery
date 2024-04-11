43ec9522db60b8025e1c7975bde58e002faa2519
CMullaney01 <conormullreid@live.com>
2024-04-11 13:39:43 +0100

    Graceful crawl shutdown

--- WebSpider/main.go
+++ WebSpider/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"webspider/fetchers"
 	"webspider/parsers"
 	"webspider/types"
@@ -9,26 +10,48 @@ import (
 
 // what do I want to crawl?
 func Crawl(urls *types.URLChanQueue, parser parsers.HTMLParser, fetcher fetchers.Fetcher) {
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	// Channel to signal when to stop processing URLs
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
 	for url := range urls.URLch {
+		// Check if the URL has been visited
 		if urls.Visited.Contains(url) {
 			continue
 		}
-		urls.Visited.Add(url)
-		go func(url string) {
-			body, err := fetcher.Fetch(url)
-			if err != nil {
-				fmt.Println("Error fetching URL:", url, err)
-				return // Skip to the next URL if there's an error
-			}
-			parser.ExtractURLs(body, urls.URLch)
-			parser.ParseBody(body)
-			fmt.Println("URL:", url)
-			fmt.Println("Body:", body)
-		}(url)
-		//prevent infinite crawling -- only visit 32 URLS
-		if urls.Visited.Size() > 32 {
+
+		// Check if we've visited 32 URLs
+		if urls.Visited.Size() >= 32 {
+			// Signal to stop processing URLs
+			close(stopCh)
 			break
 		}
+
+		// Add the URL to visited list
+		urls.Visited.Add(url)
+
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			select {
+			case <-stopCh:
+				// Stop processing if the stop signal is received
+				return
+			default:
+				body, err := fetcher.Fetch(url)
+				if err != nil {
+					fmt.Println("Error fetching URL:", url, err)
+					return // Skip to the next URL if there's an error
+				}
+				parser.ExtractURLs(body, urls.URLch)
+				parser.ParseBody(body)
+				fmt.Println("URL:", url)
+				fmt.Println("Body:", body)
+			}
+		}(url)
 	}
 }
 
